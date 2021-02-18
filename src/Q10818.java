f637394ab03e0923c578b987dfb6e560a4898f92
jchj108 <jchj108@naver.com>
2021-02-18 20:58:04 +0900

    init

---
+++ src/Q10818.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Q10818 {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		int[] arr = new int[input];
+
+		for (int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int max = arr[0];
+		int min = arr[0];
+		
+		for (int i=0; i<arr.length; i++) {
+			if(max)
+		}
+		
+	}
+
+}

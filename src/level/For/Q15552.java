43cb1bbb49c732e810efa5627891b0c4e2a5af38
jchj108 <jchj108@naver.com>
2021-05-02 03:27:41 +0900

    shortcodding

--- src/level/For/Q15552.java
+++ src/level/For/Q15552.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Q15552 {
@@ -13,7 +14,8 @@ public class Q15552 {
 
 //		token();
 //		split1();
-		split2();
+//		split2();
+		shorts();
 	}
 
 	public static void token() throws NumberFormatException, IOException {
@@ -38,30 +40,50 @@ public class Q15552 {
 
 		for (int i = 0; i < test; i++) {
 			String num[] = br.readLine().split(" ");
-
 			int a = Integer.parseInt(num[0]);
 			int b = Integer.parseInt(num[1]);
 
-			bw.write(a + b + "\n");
+			bw.write(a + b + "\n"); // 담아뒀다 한 번에 출력
 		}
+		br.close();
 		bw.flush();
 		bw.close();
 	}
 
-	private static void split2() throws IOException {
+	public static void split2() throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int test = Integer.parseInt(br.readLine());
-		String num[] = new String[test];
-		
+
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
 		for (int i = 0; i < test; i++) {
-			num[] = br.readLine().split(" ");
+			st = new StringTokenizer(br.readLine(), " ");
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
 		}
-		
-		
+		br.close();
+
+		System.out.println(sb);
+	}
+
+	public static void shorts() {
+
+		Scanner scan = new Scanner(System.in);
+		StringBuilder s = new StringBuilder();
+
+		int n = scan.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+			s.append(x + y + "\n");
 		}
 
+		System.out.print(s);
 	}
+}
 
 //	BufferedReader br = new BufferedReader(new InputStreamReader(System.in);
 //	String input = "";
